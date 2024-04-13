ca463e21afa15a0b16d76a182132c6d09f2d2027
Arjun Dev Singla <arjundevsingla1612@gmail.com>
2024-04-13 08:50:01 +0530

    Added the Images

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -12,15 +12,37 @@ const ContactForm = () => {
             <Button isOutline={false} text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>}/>
             <Button isOutline={false} text="VIA CALL" icon={<MdCall fontSize="24px"/>}/>
           </div>
+
           <Button 
             isOutline={true} 
             text="VIA EMAIL FORM" 
             icon={<HiMail fontSize="24px" />}
           />
+
+          <form>
+            <div className={styles.form_control}>
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name" />
+            </div>
+            <div className={styles.form_control}>
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" />
+            </div>
+            <div className={styles.form_control}>
+              <label htmlFor="text">Text</label>
+              <textarea name="text" rows="8"/>
+            </div>
+            <div style={{
+              dispaly: "flex",
+              justifyContent: "end",
+            }}>
+              <Button isOutline={false} text="SUBMIT" />
+            </div>
+          </form>
         </div>
 
         <div className={`${styles.contact_image}`}>
-
+            <img src="/images/contact.svg" alt="Contact Image" />
         </div>
     </section>
   )
